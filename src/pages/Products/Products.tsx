8c6cac8d2511e98b99838c276842264515571d35
ertugrulchn <cihanertugrulemre368@gmail.com>
2023-06-17 23:48:21 +0300

    feat: Add categories section

--- src/pages/Products/Products.tsx
+++ src/pages/Products/Products.tsx
@@ -6,6 +6,7 @@ import { useNavigate } from 'react-router-dom';
 
 export default function Products() {
   const [products, setProducts] = useState<ProductType[]>();
+  const [categories, setCategories] = useState<string[]>();
   const navigate = useNavigate();
 
   const fetchProducts = async () => {
@@ -18,18 +19,69 @@ export default function Products() {
     }
   };
 
+  const fetchCategories = async () => {
+    try {
+      const apiResponse = await axios.get(
+        'https://fakestoreapi.com/products/categories'
+      );
+
+      setCategories(apiResponse.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   useEffect(() => {
     fetchProducts();
+    fetchCategories();
   }, []);
 
   const handleReviewProduct = (id: number) => {
     navigate(`/products/detail/${id}`);
   };
 
+  const handleCategoryButton = async (category: string) => {
+    try {
+      const apiResponse = await axios.get(
+        `https://fakestoreapi.com/products/category/${category}`
+      );
+
+      console.log(apiResponse.data);
+
+      setProducts([...apiResponse.data]);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <main className="p-10">
+      <div className="w-full flex items-center justify-start p-5 md:p-0 md:py-5 md:justify-center overflow-y-auto space-x-4 mb-10 py-5 rounded-xl border shadow-sm">
+        {categories && (
+          <>
+            <button
+              type="button"
+              className="transition-all duration-[.3s] border p-4 rounded-xl font-medium hover:bg-black hover:text-white"
+              onClick={fetchProducts}
+            >
+              All Products
+            </button>
+
+            {categories.map((item, index) => (
+              <button
+                key={index}
+                type="button"
+                className="transition-all duration-[.3s] border p-4 rounded-xl font-medium hover:bg-black hover:text-white capitalize"
+                onClick={() => handleCategoryButton(item)}
+              >
+                {item}
+              </button>
+            ))}
+          </>
+        )}
+      </div>
       {products && (
-        <div className="grid grid-cols-4 gap-10">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-10">
           {products.map((product) => (
             <ProductCard
               key={product.id}
